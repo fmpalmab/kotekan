f067bd202e9a03154ee29a2d04c544dda9b8a83a
Keith Vanderlinde <keith.vanderlinde@utoronto.ca>
2025-02-03 16:27:52 +0000

    Added files for offline waterfall viewers.

---
+++ python/scripts/js_viewer/fetch_extlibs.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+mkdir -p extlibs
+
+wget -P extlibs http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
+wget -P extlibs http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
+wget -P extlibs http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
+wget -P extlibs http://d3js.org/d3.v3.min.js" charset="utf-8
+wget -P extlibs https://cdn.jsdelivr.net/npm/jquery-numeric@1.5.0/jquery.numeric.min.js
+wget -P extlibs https://cdn.jsdelivr.net/npm/raphael@2.3.0/raphael.min.js
+wget -P extlibs https://cdn.jsdelivr.net/npm/tinycolor2@1.6.0/cjs/tinycolor.min.js
+wget -P extlibs https://cdnjs.cloudflare.com/ajax/libs/plotly.js/2.27.1/plotly-basic.min.js
+wget -P extlibs https://cdnjs.cloudflare.com/ajax/libs/plotly.js/2.27.1/plotly-cartesian.min.js
+wget -P extlibs https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.13.6/underscore-min.js	
